d89e2f2c1745d5d0676ca77a38a912320a3d0d52
Metaboy583 <123310356+Metaboy583@users.noreply.github.com>
2023-04-26 10:52:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/c7dd70cc86cf5c548c4fe.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/9d39ddeaafffb4495aac6.jpg")
+PING_IMG = getenv("PING_IMG", "")
+START_IMG = getenv("START_IMG", "")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/GomaPeachBots")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5533865982").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 
 
 FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
